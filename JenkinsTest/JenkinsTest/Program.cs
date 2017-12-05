6063cb1d26f26525442fb8cc4459c21eb9d8b828
Wojciech Tobiś <wtobis@andea.com>
2017-12-05 13:12:24 +0100

    First change

--- JenkinsTest/JenkinsTest/Program.cs
+++ JenkinsTest/JenkinsTest/Program.cs
@@ -10,7 +10,7 @@ namespace JenkinsTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world!");
             Console.Read();
         }
     }
